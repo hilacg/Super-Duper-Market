b145142947233489675181bd352c2a0e7e893c26
Noy <noyss@mta.ac.il>
2020-09-17 11:45:24 +0300

    .

---
+++ engine/src/course/java/sdm/engine/XmlTask.java
@@ -0,0 +1,10 @@
+package course.java.sdm.engine;
+
+import javafx.concurrent.Task;
+
+public class XmlTask extends Task<Boolean> {
+    @Override
+    protected Boolean call() throws Exception {
+        return null;
+    }
+}
---
+++ javaFX/src/scenes/superScene/SuperScene.java
@@ -0,0 +1,15 @@
+package scenes.superScene;
+
+import course.java.sdm.engine.Engine;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+
+public class SuperScene  extends Scene {
+
+    private Engine engine;
+
+    public SuperScene(Engine engine){
+        super(new FlowPane(), 500, 400);
+        this.engine = engine;
+    }
+}
